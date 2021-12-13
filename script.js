bd5f929af3a62a3d24daf4247489ed15cfddaf70
sidhartha182 <67259337+sidhartha182@users.noreply.github.com>
2021-12-13 15:13:59 +0530

    Update script.js
    pm

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Engineer", "Developer", "Speaker", "Teacher", "Freelancer"],
+        strings: ["Engineer", "Developer", "Speaker", "Teacher", "Product Manager"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Engineer", "Developer", "Speaker", "Teacher", "Freelancer"],
+        strings: ["Engineer", "Developer", "Speaker", "Teacher", "Product Manager"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
